712dd24da904154c5104e5bf162f9282533eba63
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-02-21 16:45:09 +0400

    fixed authenticator

--- src/authenticator.erl
+++ src/authenticator.erl
@@ -5,6 +5,7 @@
 sign(SignString, Secret) ->
   string:to_lower(hmac:hexlify(hmac:hmac256(Secret, SignString))).
 
-can_join({private, ChannelName, SocketId, Auth, CustomString}) ->
-  SignStr = SocketId ++ ":" ++ binary_to_list(ChannelName) ++ ":" ++ CustomString
+can_join(private, ChannelName, SocketId, Auth, CustomString) ->
+  SignString = SocketId ++ ":" ++ binary_to_list(ChannelName) ++ ":" ++ CustomString,
+  Secret = "123",
   Auth = sign(SignString, Secret).
