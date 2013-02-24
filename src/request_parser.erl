c1733797701bb31dcea2ddca949f71cf559c2bcd
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-02-24 23:52:50 +0400

    removed mochijson2

--- src/request_parser.erl
+++ src/request_parser.erl
@@ -3,11 +3,11 @@
 -export([get_channel_name/1, get_action_name/1, get_pid_from_req/1]).
 
 get_channel_name(Data) ->
-  {struct, [_|[{<<"data">>, {struct, [{<<"channel">>, Channel_name}|_]}}|_]]} = mochijson2:decode(binary_to_list(Data)),
+  {[_|[{<<"data">>, {[{<<"channel">>, Channel_name}|_]}}|_]]} = jiffy:decode(Data),
   Channel_name.
 
 get_action_name(Data) ->
-  {struct, [{_, ActionName}|_]} = mochijson2:decode(binary_to_list(Data)),
+  {[{_, ActionName}|_]} = jiffy:decode(Data),
   ActionName.
 
 get_pid_from_req(Req) ->
