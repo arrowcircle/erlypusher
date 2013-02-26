8579297043638e606dcfb6a3b2906a8c917fca43
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-02-26 22:34:22 +0400

    playn with jiffy

--- src/json_responder.erl
+++ src/json_responder.erl
@@ -19,6 +19,8 @@ response({ping}) ->
 % errors
 
 response({error_no_app, AppId}) ->
+  % Message = <<"Could not find app by key ">> + AppId;
+  % jiffy:encode({[{<<"event", <<"pusher:error">>}, {<<"data">>, {[{<<"code">>, 4001}, {<<"message">>, Message}]}}]});
   "{\"event\":\"pusher:error\",\"data\":{\"code\":4001,\"message\":\"Could not find app by key " ++ binary_to_list(AppId) ++ "\"}}";
 
 response(_Any) ->
