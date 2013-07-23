9ceebed483b3486a5f0fd5b6192097129bc58d85
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-07-23 21:48:51 +0400

    Added presence to erlypusher start

--- src/erlypusher_app.erl
+++ src/erlypusher_app.erl
@@ -14,31 +14,38 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    erlypusher_config:prepare(),
-    case application:get_env(erlypusher, port) of
-      {ok, Port} ->
-        Port;
-      _ ->
-        Port = 8080
-    end,
-    Dispatch = cowboy_router:compile([
-                    %% {HostMatch, list({PathMatch, Handler, Opts})}
-                    {'_', [{"/app/:key", erlypusher_websocket_handler, []},
-                           {"/", main_page, []},
-                           {"/apps/:app_id/channels/:channel_id/events", erlypusher_api_handler, []},
-                           {"/apps/:app_id/events", erlypusher_api_handler, []},
-                           {"/apps/:app_id/channels", erlypusher_channels_handler, []},
-                           {"/apps/:app_id/channels/:channel_id", erlypusher_channels_handler, []},
-                           {"/timeline/:id", erlypusher_timeline_handler, []}
-                          ]}
-                ]),
-    cowboy:start_http(my_http_listener, 100,
-        [{port, Port}],
-        [{env, [{dispatch, Dispatch}]},
-         {}
-        ]
-    ),
-    erlypusher_sup:start_link().
+  
+  erlypusher_presence:start(),
+
+  Port = get_port(),  
+    
+  Dispatch = cowboy_router:compile([
+                  %% {HostMatch, list({PathMatch, Handler, Opts})}
+                  {'_', [{"/app/:key", erlypusher_websocket_handler, []},
+                         {"/", main_page, []},
+                         {"/apps/:app_id/channels/:channel_id/events", erlypusher_api_handler, []},
+                         {"/apps/:app_id/events", erlypusher_api_handler, []},
+                         {"/apps/:app_id/channels", erlypusher_channels_handler, []},
+                         {"/apps/:app_id/channels/:channel_id", erlypusher_channels_handler, []},
+                         {"/timeline/:id", erlypusher_timeline_handler, []}
+                        ]}
+              ]),
+  cowboy:start_http(my_http_listener, 100,
+      [{port, Port}],
+      [{env, [{dispatch, Dispatch}]},
+       {}
+      ]
+  ),
+  erlypusher_sup:start_link().
 
 stop(_State) ->
     ok.
+
+get_port() ->
+  erlypusher_config:prepare(),
+  case application:get_env(erlypusher, port) of
+    {ok, Port} ->
+      Port;
+    _ ->
+      8080
+  end.
