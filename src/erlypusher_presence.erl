1a3b8141613c247aa48ce435398cb5f25da4e088
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-07-23 18:24:01 +0400

    Changed presence to genserver structure

--- src/erlypusher_presence.erl
+++ src/erlypusher_presence.erl
@@ -1,16 +1,67 @@
 -module(erlypusher_presence).
 
+-behaviour(gen_server).
+
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([store/0, start/0]).
 -compile(export_all).
 
--define(PRESENCE, erlypusher_presence).
+-record(presence, {app_id, channel_id, uuid, info, created_at, pid}).
 
--record(presence, {app_id, channel_id, uuid, info, created_at}).
+%% Public API
+
+store() ->
+  gen_server:call(?MODULE, {msg}).
 
 start() ->
-  server_util:start(?PRESENCE, {erlypusher_presence, run, [true]}).
+  gen_server:start({global, ?MODULE}, ?MODULE, [], []).
 
 stop() ->
-  server_util:stop(?PRESENCE).
+  gen_server:call(?MODULE, stop).
+
+state() ->
+  gen_server:call(?MODULE, state).
+
+
+%% Server implementation
+
+init([]) ->
+  init_presence(),
+  {ok, []}.
+
+handle_call({subscribe, UserInfo, AppId}, _From, State) ->
+  io:format("\n~p: Received subscribtion event!: ~p\n", [AppId, UserInfo]),
+  {reply, ok, State};
+
+handle_call({list, ChannelId, AppId}, _From, State) ->
+  io:format("\n~p: Received list event: ~p\n", [AppId, ChannelId]),
+  {reply, ok, State};
+
+handle_call({unsubscribe, UserId, AppId}, _From, State) ->
+  io:format("\n~p: Received unsubscribe event: ~p\n", [AppId, UserId]),
+  {reply, ok, State};
+
+handle_call(stop, _From, State) ->
+  mnesia:stop(),
+  {stop, normal, stopped, State};
+
+handle_call(_Request, _From, State) ->
+  {reply, ignored_message, State}.
+
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%% Internal functions
 
 init_presence() ->
   mnesia:create_schema(node()),
@@ -25,26 +76,3 @@ init_presence() ->
           {ram_copies, node()}
       ])
   end.
-
-run(FirstTime) ->
-  if 
-    FirstTime == true ->
-      init_presence(),
-      run(false);
-    true ->
-      receive
-        {subscribe, UserInfo, AppId} ->
-          io:format("\n~p: Received subscribtion event: ~p\n", [AppId, UserInfo]),
-          run(FirstTime);
-        {list, ChannelId, AppId} ->
-          io:format("\n~p: Received list event: ~p\n", [AppId, ChannelId]),
-          run(FirstTime);
-        {unsubscribe, UserId, AppId} ->
-          io:format("\n~p: Received unsubscribe event: ~p\n", [AppId, UserId]),
-          run(FirstTime);
-        _ ->
-          io:format("\n: Received unknown event\n"),
-          run(FirstTime)
-      end
-    end.
-
